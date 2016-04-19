9124357c8586b2ba0f7db80be6d644266e9a26b7
Will Wheeler <willfulbard@gmail.com>
2016-04-19 15:04:07 -0700

    Add passport to the server

--- server/server.js
+++ server/server.js
@@ -5,21 +5,34 @@ var app = express();
 var port = process.env.PORT || 7777;
 var routes = require('./controllers/routes.js');
 var db = require('./db/index.js');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+//Must create this!
+var apiKeys = require('./utility/apikeys.js');
 
+passport.use(
+  new GoogleStrategy({
+    clientID: apiKeys.google.key,
+    clientSecret: apiKeys.google.secret,
+    callbackURL: "http://localhost/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  })
+);
 
 /* Static */
-
 app.use(express.static(path.join(__dirname, "../client")));
-// app.use(express.static(path.join(__dirname, "../lib")));
+
 /* middlewares */
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-// app.set('views', path.join(__dirname, '../client/'));
-
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
-
+app.use(passport.initialize());
+app.use(passport.session());
+ 
 routes(app);
 
 app.listen(port, function() {

59b7cf1c64983bdc80b2f3b9dc19fdd304a59e10
Travis Carson <travislcarson@gmail.com>
2016-04-19 19:29:34 -0700

    add dropdown html

--- client/app/addimage/addimage.js
+++ client/app/addimage/addimage.js
@@ -1,5 +1,10 @@
 // David
 // Controller for adding items
 angular.module('e-Commer.addImage', ['ngMaterial'])
-.controller('addImageController', function () {
+.controller('addImageController', function ($scope) {
+
+  $scope.uploadFiles = function() {
+    console.log('UPLOAD FILES CALLED');
+  };
+
 });

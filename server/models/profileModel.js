ccf4a0387fa5710cd4d2224a4f2af6a9db9092d8
Alexander James Turinske <a.turinske@gmail.com>
2016-04-19 20:16:39 -0700

    Start to implement a query to receive reviews

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -8,7 +8,10 @@ module.exports = {
     console.log('userId ', userId);
     
     var queryRecentItems = 'select i.id,i.name,i.description,i.price,s.name as user from items i inner join user_items ui on ui.item_Id = i.id inner join users s on ui.user_Id = s.id where ui.user_Id = '+userId;
-
+    var queryItemReviews = 'SELECT i.name, r.user_experience, r.item_rating, u.name FROM items i INNER JOIN reviews r ON i.id=r.id_items INNER JOIN users u ON r.id_users=u.id'
+    db.query(queryItemReviews, function(err, results) {
+      console.log('item review results', results);
+    });
     db.query(queryRecentItems, function(err, results) {
       console.log('profile query err',err);
       resultObj.items4rent = results;

1375c1643b579502e310dc1d51edefe01a9f2a0a
Alexander James Turinske <a.turinske@gmail.com>
2016-04-20 12:27:46 -0700

    Refactor some query functions and implement feature where reviews get returned with the items

--- server/models/profileModel.js
+++ server/models/profileModel.js
@@ -8,22 +8,37 @@ module.exports = {
     console.log('userId ', userId);
     
     var queryRecentItems = 'select i.id,i.name,i.description,i.price,s.name as user from items i inner join user_items ui on ui.item_Id = i.id inner join users s on ui.user_Id = s.id where ui.user_Id = '+userId;
-    var queryItemReviews = 'SELECT i.name, r.user_experience, r.item_rating, u.name FROM items i INNER JOIN reviews r ON i.id=r.id_items INNER JOIN users u ON r.id_users=u.id'
-    db.query(queryItemReviews, function(err, results) {
-      console.log('item review results', results);
-    });
     db.query(queryRecentItems, function(err, results) {
-      console.log('profile query err',err);
-      resultObj.items4rent = results;
-      console.log('profile query results',resultObj);
+      if (err) {
+        console.log('profile query err',err);
+      } else {
+        console.log('items4rent query results', results);
+        resultObj.items4rent = results;
+        // get all he reviews for the items
+        resultObj.items4rent.forEach(function(e) {
+          console.log('yisss: ', e);
+          var queryItemReviews = 'SELECT i.name AS item, r.user_experience, r.item_rating, u.name AS user FROM items i INNER JOIN reviews r ON i.id=r.items_Id INNER JOIN users u ON r.users_Id=u.id WHERE i.name = \'' + e.name + '\';';
+          db.query(queryItemReviews, function(err, results) {
+            if (err) {
+              console.log('item review err: ', err);
+            } else {
+              console.log('yiss:', e);
+              e.reviews = results;
+              console.log('yiss:', e.reviews);
+            }
+          });
+        });
+      }
 
       var queryRentingItems = 'select i.id,i.name,i.description,i.price,s.name from items i inner join items_renting ri on ri.item_Id = i.id inner join users s on ri.user_Id = s.id where ri.user_Id = '+userId;
-
       db.query(queryRentingItems, function(err, results) {
-        console.log('profile query err',err);
-        resultObj.itemsRenting = results;
-        console.log('profile query results',resultObj);
-        callback(err, resultObj);
+        if (err) {
+          console.log('profile query err',err);
+        } else {
+          console.log('profile query results: ', resultObj);
+          resultObj.itemsRenting = results;
+          callback(err, resultObj);
+        }
       });
     });
   }

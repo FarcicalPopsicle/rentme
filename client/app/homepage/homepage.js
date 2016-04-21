4c8cf8eca00bc5181a0bd842c207e7fe47660afd
Alexander James Turinske <a.turinske@gmail.com>
2016-04-21 10:20:07 -0700

    Implement reviews for items on the homepage using logic on the frontend

--- client/app/homepage/homepage.js
+++ client/app/homepage/homepage.js
@@ -24,22 +24,28 @@ app.controller('SearchController', function ($scope,$location, $window, searchFa
   $scope.checkOutCounter = 0;
   $scope.user = Auth.user;
 
-  searchFactory.getSearchItems({default:'default'}).then(function(items){
+  searchFactory.getSearchItems({ default: 'default' }).then(function (items) {
     $scope.randItemsData = items.randItems;
-    console.log('items from server: ', items);
-    // TODO add logic here to append reviews to randItemsData
+    for (let i = 0; $scope.randItemsData[i]; i++) {
+      $scope.randItemsData[i].reviews = [];
+      for (let j = 0; items.randItems[j]; j++) {
+        if ($scope.randItemsData[i].name === items.reviews[j].item) {
+          $scope.randItemsData[i].reviews.push(items.reviews[j]);
+        }
+      }
+    }
   });
 
-  $scope.logout = function (){
+  $scope.logout = function () {
     Auth.signout();
-  }
+  };
  
-  $scope.addToCheckOut = function(item) {
+  $scope.addToCheckOut = function (item) {
     $scope.checkOutItems.push(item);
-    $scope.checkOutCounter++
-  }
+    $scope.checkOutCounter++;
+  };
 
-  $scope.processCheckOut = function() {
+  $scope.processCheckOut = function () {
     //Send the Cart to the server
     console.log('transfering you to paypal server..');
     var checkoutData = {id:$scope.user.id, items:$scope.checkOutItems};

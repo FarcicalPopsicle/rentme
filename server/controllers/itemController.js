402733819b441b81b3daac73ecbee3d935a4d1c0
Travis Carson <travislcarson@gmail.com>
2016-04-20 17:25:33 -0700

    receive image, create new directory based on user name, save file

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -1,5 +1,7 @@
 // Sylvia
 var Items = require('../models/itemModel.js');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 
 // choose function below
@@ -16,13 +18,44 @@ module.exports = {
 
   
   addItem: function (req, res, next) {
-    console.log('Inside addItem method; request body: ', req.body);
-    Items.items(req.body,function(err, results) {
-      if (!err) { 
-        res.json(201);
+    var photo = req.body.item.photos[0];
+    var userid = req.body.id;
+    console.log('Inside addItem method; photo: ', photo);
+
+    var pathToCreate = './server/assets/images/' + userid + '/';
+    var writePath = './server/assets/images/' + userid + '/' + 'eggs.jpg';
+
+    mkdirp(pathToCreate, function(err) {
+      if (err) {
+        console.log('error creating path: ', err);
       } else {
-        res.json(err);
+        console.log('--------> NEW PATH CREATED <----------');
+        fs.writeFile(writePath, photo, function(err) {
+          if (err) {
+            console.log('error saving image: ', err);
+          } else {
+            console.log('image saved to: ', writePath);
+          }
+        });     
       }
     });
+
+    // fs.writeFile(path, req.body.item.photos[0], function(err) {
+    //   if (err) {
+    //     console.log('error saving image: ', err);
+    //   } else {
+    //     console.log('image saved to: ', path);
+    //   }
+    // });
+
+
+
+    // Items.items(req.body,function(err, results) {
+    //   if (!err) { 
+    //     res.json(201);
+    //   } else {
+    //     res.json(err);
+    //   }
+    // });
   },
 };

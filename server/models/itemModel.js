3d06f2915ee27d2f02a12f113ff112bf73ac7a4b
Alexander James Turinske <a.turinske@gmail.com>
2016-04-20 17:14:22 -0700

    Modify item model to handle errors

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -2,13 +2,21 @@ var db = require('../db');
 
 module.exports = {
   items: function (userItem,callback) {
-    var query = 'insert into items (name,description,price) values ("'+userItem.item.name+'","'+userItem.item.description+'",'+userItem.item.price+')';
+    var query = 'insert into items (name,description,price) values ("'
+      + userItem.item.name + '","'
+      + userItem.item.description + '",'
+      + userItem.item.price + ')';
     db.query(query, function(err, results) {
-      query = 'insert into user_items (user_Id,item_Id) values ('+ userItem.id +',(select id from items where name = "'+userItem.item.name+'" Limit 1))';
+      query = 'insert into user_items (user_Id,item_Id) values ('
+        + userItem.id + ',(select id from items where name = "'
+        + userItem.item.name + '" Limit 1))';
       db.query(query, function(err, results) {
-        console.log('Item', results);
-        console.log('err Item', err);
-        callback(err, results);
+        if (err) {
+          console.log('err Item', err);
+        } else {
+          console.log('Item', results);
+          callback(err, results);
+        }
       });
     });
   }

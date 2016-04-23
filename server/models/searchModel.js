895fcdf478d6363887a39e3fb5cea65d87607609
Will Wheeler <willfulbard@gmail.com>
2016-04-22 20:56:39 -0700

    Fix error and remove console logs

--- server/models/searchModel.js
+++ server/models/searchModel.js
@@ -16,10 +16,10 @@ module.exports = {
         } else {
           resultsObj.randItems = results;
           // get all the reviews for all of the items
-          var queryItemReviews = 'SELECT i.name AS item, r.user_experience, r.item_rating, u.name'
+          var queryItemReviews = 'SELECT i.id, i.name AS item, r.user_experience, r.item_rating, u.name'
             + ' AS user FROM items i INNER JOIN reviews r ON i.id=r.items_Id INNER JOIN users u'
-            + ' ON r.users_Id=u.id';
-          if (!results) {
+            + ' ON r.users_Id=u.id ';
+          if (results) {
             queryItemReviews += 'WHERE i.id = ' + resultsObj.randItems[0].id;
             for (var i = 1; results[i]; i++) {
               queryItemReviews += ' OR i.id = ' + resultsObj.randItems[i].id;
@@ -38,7 +38,6 @@ module.exports = {
       });
     },
     getAllItems: function (userSearch, callback) {
-      console.log('Inside Model', userSearch);
       if(userSearch.hasOwnProperty('item')){
         var queryItems = 'select i.id,i.name,i.description,i.photo,i.price,i.availability,a.city from items i inner join user_items ui on ui.item_Id = i.id inner join users s on ui.user_Id = s.id inner join address a on a.id  = s.address_id and a.city ="'+ userSearch.city+'" where i.name = "'+userSearch.item+'"';
       } else {
